62111400360a48e7563ee806f07e8830c3d17977
Daniela Cuevas <danielacuevasd@gmail.com>
2026-07-11 11:26:13 -0400

    feat: se agrega @EnableDiscoveryClient a todos los Application.java faltantes

--- ms-mascotas/src/main/java/com/veterinaria/ms_mascotas/MsMascotasApplication.java
+++ ms-mascotas/src/main/java/com/veterinaria/ms_mascotas/MsMascotasApplication.java
@@ -2,8 +2,10 @@ package com.veterinaria.ms_mascotas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsMascotasApplication {
 
 	public static void main(String[] args) {
--- ms-notificaciones/src/main/java/com/veterinaria/ms_notificaciones/MsNotificacionesApplication.java
+++ ms-notificaciones/src/main/java/com/veterinaria/ms_notificaciones/MsNotificacionesApplication.java
@@ -2,8 +2,10 @@ package com.veterinaria.ms_notificaciones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsNotificacionesApplication {
 
 	public static void main(String[] args) {
